4fc8cc36e78fc249cc4caeb035f0b90d4e9c8417
NagLau <naglau747@hengersor.hu>
2025-01-22 09:20:35 +0100

    orai munka

---
+++ react/reactfilozofia/app.js
@@ -0,0 +1,12 @@
+//natív->hogyan? 
+const btn = document.createElement('button');
+
+btn.onclick = function()
+{
+    alert ("Ez egy natív kód");
+}
+
+btn.innerHTML = "Natív gomb";
+
+document.getElementById("nativ-button-container").appendChild(btn);
+
