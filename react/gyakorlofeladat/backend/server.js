bbc407e4f89f62de51fe6fab465a9f4adadcb189
NagLau <naglau747@hengersor.hu>
2025-01-24 13:21:41 +0100

    utolso frissites ma (01.24)

--- react/gyakorlofeladat/backend/server.js
+++ react/gyakorlofeladat/backend/server.js
@@ -1,33 +1,32 @@
 const express = require ("express");
-const app = express();
-const cors = require("cors");
+const app = express ();
+const cors = require ("cors");
 const mysql = require("mysql");
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
-
+ 
 app.use(cors());
-
+ 
 const db = mysql.createConnection({
     user: "root",
-    host: "127.0.0.1",
-    port: 3307,
-    password: "",
-    database: kozutak,
+    host:"127.0.0.1",
+    port:3307,
+    password:"",
+    database: "kozutak",
 });
-
+ 
 app.get("/", (req,rest) => {
     rest.send("Fut a backend!");
-});
-
-app.get("/regiok", (req, res) => {
-    const sql = "SELECT * FROM 'regiok'";
-    db.query(sql, (err, result) => {
-        if (err) return req.json(err);
+})
+ 
+app.get ("/regiok",( req, res) => {
+    const sql ="SELECT * FROM `regiok`";
+    db.query(sql, (err,result) => {
+        if (err) return res.json(err);
         return res.json(result)
     })
 })
-
+ 
 app.listen(3001, () => {
     console.log("Server is running on port 3001");
-});
-
+})
